5efe61f3fb7d6cf552bf66b41f993f7fb9abe1af
Josephine Ariella <57031120+josephineariela@users.noreply.github.com>
2020-03-07 02:58:51 +0000

    Delete SQL_toMQTT.py

--- SQL_toMQTT.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-result = subprocess.Popen(["php /var/www/html/SQL_toESP-A.php"], shell=True, stdout=subprocess.PIPE)
-
-script_response = result.stdout.read()
-print(script_response)
